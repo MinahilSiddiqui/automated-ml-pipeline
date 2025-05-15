518f2e0c213673ad7dbd265da6776a2ef3797ca2
MinahilSiddiqui <manahalsiddiqui@gmail.com>
2025-05-15 23:09:30 +0500

    replacing with a single ipynb file

--- data_loader.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_iris
-import pandas as pd
-
-def load_data():
-    data = load_iris()
-    df = pd.DataFrame(data.data, columns=data.feature_names)
-    df['target'] = data.target
-    return df
--- tests/test_model.py
+++
@@ -1,7 +0,0 @@
-from data_loader import load_data
-from train import train_model
-
-def test_model_accuracy():
-    df = load_data()
-    acc = train_model(df)
-    assert acc > 0.8, "Accuracy should be greater than 80%"
